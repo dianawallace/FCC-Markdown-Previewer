b2b2f88e478607aa1b7b31e45c5665b04d17b575
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-07-08 17:40:20 -0600

    added editor and preview  textarea

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
+const initialMarkdown = `
+### Headers
+
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+
+`
+
 class App extends React.Component {
     render() {
         return (
-            <div>Markdown Previewer</div>
+            <div>
+                <div>Markdown Previewer</div>
+                <div className='container'>
+                    <div className='left'>
+                    </div>
+                        <textarea id='editor'/>
+                    <div className='right'>
+                        <textarea id='preview'/>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
